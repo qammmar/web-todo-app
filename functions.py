637a2649f47985fc58f2658f8e1f5f48abf6f6d3
Your Name <you@example.com>
2024-07-27 20:57:35 +0500

    "implemented te add button"

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file_local:
             todos_local = file_local.readlines()
     return todos_local
--- gui.py
+++ gui.py
@@ -2,9 +2,22 @@ import FreeSimpleGUI as sg
 import functions
 
 label = sg.Text("type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-windows = sg.Window('To-Do App', layout=[[label], [input_box, add_button]])
-windows.read()
+windows = sg.Window('To-Do App', 
+                    layout=[[label], [input_box, add_button]], 
+                    font=('Helvetica', 20))
+while True:
+    event, values = windows.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 windows.close()
